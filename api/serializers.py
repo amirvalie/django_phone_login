b16dd312015691562ffda6c8389c402c167f2545
amirvalie <hosseinvalie@gmail.com>
2023-07-03 23:29:09 +0330

    Compliance with PEP principles

--- api/serializers.py
+++ api/serializers.py
@@ -3,14 +3,17 @@ from rest_framework import serializers
 
 
 def validate_phone_number(value):
-    regex='09(\d{9})$'
+    regex = '09(\d{9})$'
     if re.search(regex, value):
         return value
     else:
         raise serializers.ValidationError("invalid phone number")
 
+
 class PhoneTokenSerializer(serializers.Serializer):
     phone_number = serializers.CharField(max_length=11, validators=[validate_phone_number], required=True)
 
 
-
+class ConfirmTokenSerializer(serializers.Serializer):
+    phone_number = serializers.CharField(max_length=11, validators=[validate_phone_number], required=True)
+    otp = serializers.IntegerField()
