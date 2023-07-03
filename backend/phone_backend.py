233d87e29b2ec78bb63a29c53ad3199d23c78b92
amirvalie <hosseinvalie@gmail.com>
2023-07-03 23:26:11 +0330

    Compliance with PEP principles

--- backend/phone_backend.py
+++ backend/phone_backend.py
@@ -6,11 +6,12 @@ import pytz
 from django_phone_login.models import PhoneToken
 from django.utils import timezone
 
-User=get_user_model()
+User = get_user_model()
+
 
 class PhoneBackend(ModelBackend):
 
-    def get_or_create_user(self,phone_number):
+    def get_or_create_user(self, phone_number):
         try:
             return User.objects.get(phone=phone_number)
         except User.DoesNotExist:
@@ -19,26 +20,27 @@ class PhoneBackend(ModelBackend):
                 phone=phone_number,
                 is_active=True
             )
-    def authenticate(self,request,otp,phone_number):
+
+    def authenticate(self, request, otp, phone_number):
         """
         we check the phone number and SMS OTP and the time validity of the SMS OTP.
         If the information is correct, a user will be created or taken with the given phone number
         """
         try:
-            expire_time=getattr(settings, 'EXPIRE_TIME', 10)
-            diffrence_time=timezone.now() + datetime.timedelta(minutes=expire_time)
-            phone_token=PhoneToken.objects.get(
+            expire_time = getattr(settings, 'EXPIRE_TIME', 10)
+            diffrence_time = timezone.now() + datetime.timedelta(minutes=expire_time)
+            phone_token = PhoneToken.objects.get(
                 phone_number=phone_number,
-                otp = otp,
+                otp=otp,
                 timestamp__lte=diffrence_time,
-                used = False
+                used=False
             )
         except PhoneToken.DoesNotExist:
             return None
-        user=self.get_or_create_user(phone_number)
+        user = self.get_or_create_user(phone_number)
         if self.user_can_authenticate(user):
-            return user 
-        return         
+            return user
+        return
 
     def user_can_authenticate(self, user):
         """
