7b0d0685d78a0130eebd63560436c239d8aac613
amirvalie <hosseinvalie@gmail.com>
2023-07-03 23:21:42 +0330

    Added ConfirmTokenApi view to check otp and authenticate user

--- api/phone_login_apis.py
+++ api/phone_login_apis.py
@@ -1,11 +1,17 @@
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from .serializers import PhoneTokenSerializer
 from ..models import PhoneToken
 from drf_spectacular.utils import extend_schema
-
+from django.contrib.auth import (
+    authenticate, login
+)
+from .serializers import PhoneTokenSerializer, ConfirmTokenSerializer
+from .permissions import IsNotAuthenticated
+from ..limit_request import GenerateLimitation
 
 class GenerateTokenApi(APIView):
+    permission_classes = [IsNotAuthenticated]
 
     @extend_schema(request=PhoneTokenSerializer)
     def post(self, request, *args, **kwargs):
@@ -14,8 +20,33 @@ class GenerateTokenApi(APIView):
         phone_token = PhoneToken.objects.get_or_create(
             phone_number=serializer.validated_data.get('phone_number'),
         )[0]
-        print
-        token = PhoneToken.send_otp(phone_token)
+        PhoneToken.send_otp(phone_token)
+        GenerateLimitation(request)
         return Response(
-            {'OTP code': token}
+            {'otp': phone_token.otp},
+            status=status.HTTP_200_OK
+        )
+
+
+class ConfirmTokenApi(APIView):
+
+    @extend_schema(request=ConfirmTokenSerializer)
+    def post(self, request):
+        serializer = ConfirmTokenSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        print(serializer.validated_data.get('otp'))
+        user = authenticate(
+            self.request,
+            otp=serializer.validated_data.get('otp'),
+            phone_number=serializer.validated_data.get('phone_number'),
+        )
+        if user is not None:
+            login(self.request, user, backend='django.contrib.auth.backends.ModelBackend')
+            return Response(
+                'User authenticated successfully',
+                status=status.HTTP_200_OK
+            )
+        return Response(
+            'otp is not valid',
+            status=status.HTTP_400_BAD_REQUEST
         )
