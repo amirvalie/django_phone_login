aefb9ec1762368b419c84b82ed8226f80695cefe
amirvalie <hosseinvalie@gmail.com>
2023-10-05 13:54:11 +0330

    Using black formating extention for formating the project

--- limit_request.py
+++ limit_request.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.core.cache import cache
 
+
 class GenerateLimitation(object):
     """
     We use cache to limit user requests.
@@ -9,32 +10,35 @@ class GenerateLimitation(object):
     and in the next part, which is a decorator,we check whether the number of user requests is valid or not.
     This method allows us to use this class wherever we want.
     """
-    expire_cache=getattr(settings,'EXPIRE_CACHE',20)
-    def __init__(self,request):
-        self.ip_address=request.user.ip_address
+
+    expire_cache = getattr(settings, "EXPIRE_CACHE", 20)
+
+    def __init__(self, request):
+        self.ip_address = request.user.ip_address
         self.get_or_set()
 
     def get_or_set(self):
-        obj=cache.get_or_set(self.ip_address,0,self.expire_after())
+        obj = cache.get_or_set(self.ip_address, 0, self.expire_after())
         self.inc_cache()
-    
+
     def inc_cache(self):
         cache.incr(self.ip_address)
 
     def expire_after(self):
         return GenerateLimitation.expire_cache * 60
-        
 
 
 def check_limitation(func):
     """
     in this decorator we check whether the number of user requests is valid or not
     """
+
     def wrapper(request):
         from .views import invalid_attempts_massage_view
-        attempts=cache.get(request.user.ip_address)
-        if attempts is None or attempts < getattr(settings,'PHONE_LOGIN_ATTEMPTS',10):
+
+        attempts = cache.get(request.user.ip_address)
+        if attempts is None or attempts < getattr(settings, "PHONE_LOGIN_ATTEMPTS", 10):
             return func(request)
         return invalid_attempts_massage_view(request)
-    return wrapper
 
+    return wrapper
--- views.py
+++ views.py
@@ -13,6 +13,7 @@ from django.utils.decorators import method_decorator
 from django.urls import reverse_lazy
 from django.core.exceptions import ValidationError
 from django.contrib.auth import get_user_model
+
 User = get_user_model()
 from .decorators import (
     password_require,
@@ -33,16 +34,17 @@ def invalid_attempts_massage_view(request):
     This function is executed when the number of user attempts to login
     or enter the code exceeds the allowed limit
     """
-    return render(request, 'registration/invalid_attempts.html')
+    return render(request, "registration/invalid_attempts.html")
 
 
 class GenerateToken(FormView):
     """
     Create or get PhoneToken object and send a SMS OTP
     """
+
     form_class = PhoneTokenForm
-    template_name = 'registration/generate_token.html'
-    success_url = reverse_lazy('phone_login:confirm_otp')
+    template_name = "registration/generate_token.html"
+    success_url = reverse_lazy("phone_login:confirm_otp")
 
     def post(self, request, *args, **kwargs):
         form = self.form_class(data=request.POST)
@@ -53,7 +55,7 @@ class GenerateToken(FormView):
 
     def form_valid(self, form):
         GenerateLimitation(self.request)
-        self.request.session['phone_number'] = form.phone_token.phone_number
+        self.request.session["phone_number"] = form.phone_token.phone_number
         PhoneToken.send_otp(form.phone_token)
         return super().form_valid(form)
 
@@ -65,31 +67,8 @@ class GenerateToken(FormView):
 
 class ConfirmToken(FormView):
     form_class = PhoneTokenConfirmForm
-    template_name = 'registration/confirm_phone.html'
-    success_url = '/'
-
-    def post(self, request, *args, **kwargs):
-        form = self.form_class(request=request, data=request.POST)
-        if form.is_valid():
-            return self.form_valid(form)
-        else:
-            return self.form_invalid(form)
-
-    def form_valid(self, form):
-        login(self.request, form.user_cache, backend='django.contrib.auth.backends.ModelBackend')
-        del self.request.session['phone_number']
-        return super().form_valid(form)
-
-    @method_decorator(phone_number_require('login', 'phone_number'))
-    @method_decorator(check_limitation)
-    def dispatch(self, request, *args, **kwargs):
-        return super().dispatch(request, *args, **kwargs)
-
-
-class PasswordLogin(FormView):
-    form_class = PasswordLoginForm
-    template_name = 'registration/password_login.html'
-    success_url = '/'
+    template_name = "registration/confirm_phone.html"
+    success_url = "/"
 
     def post(self, request, *args, **kwargs):
         form = self.form_class(request=request, data=request.POST)
@@ -100,13 +79,41 @@ class PasswordLogin(FormView):
 
     def form_valid(self, form):
         login(
-            self.request, form.user_cache,
-            backend='django.contrib.auth.backends.ModelBackend'
+            self.request,
+            form.user_cache,
+            backend="django.contrib.auth.backends.ModelBackend",
         )
-        del self.request.session['phone_number']
+        del self.request.session["phone_number"]
         return super().form_valid(form)
 
-    @method_decorator(phone_number_require('login', 'phone_number'))
+    @method_decorator(phone_number_require("login", "phone_number"))
+    @method_decorator(check_limitation)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+
+
+class PasswordLogin(FormView):
+    form_class = PasswordLoginForm
+    template_name = "registration/password_login.html"
+    success_url = "/"
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request=request, data=request.POST)
+        if form.is_valid():
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(form)
+
+    def form_valid(self, form):
+        login(
+            self.request,
+            form.user_cache,
+            backend="django.contrib.auth.backends.ModelBackend",
+        )
+        del self.request.session["phone_number"]
+        return super().form_valid(form)
+
+    @method_decorator(phone_number_require("login", "phone_number"))
     @method_decorator(check_limitation)
     @method_decorator(password_require)
     def dispatch(self, *args, **kwargs):
@@ -115,8 +122,8 @@ class PasswordLogin(FormView):
 
 class ForgetPassword(FormView):
     form_class = ForgetPasswordForm
-    template_name = 'registration/forget_password.html'
-    success_url = reverse_lazy('phone_login:confirm_password_otp')
+    template_name = "registration/forget_password.html"
+    success_url = reverse_lazy("phone_login:confirm_password_otp")
 
     def post(self, request, *args, **kwargs):
         form = self.form_class(request=request, data=request.POST)
@@ -128,7 +135,9 @@ class ForgetPassword(FormView):
     def form_valid(self, form):
         GenerateLimitation(self.request)
         generate_token = PhoneToken.send_otp(form.phone_token)
-        self.request.session['phone_number_for_password'] = form.phone_token.phone_number
+        self.request.session[
+            "phone_number_for_password"
+        ] = form.phone_token.phone_number
         return super().form_valid(form)
 
     @method_decorator(check_limitation)
@@ -139,52 +148,66 @@ class ForgetPassword(FormView):
 
 class ConfirmPasswordToken(FormView):
     form_class = PhoneTokenConfirmForm
-    template_name = 'registration/confirm_phone.html'
+    template_name = "registration/confirm_phone.html"
 
     def post(self, request, *args, **kwargs):
         form = self.form_class(request=request, data=request.POST)
         if form.is_valid():
-            print('form is valid')
+            print("form is valid")
             return self.form_valid(form)
         else:
-            print('form is not valid')
+            print("form is not valid")
             return self.form_invalid(form)
 
     def form_valid(self, form):
-        phone_number = self.request.session.get('phone_number_for_password')
+        phone_number = self.request.session.get("phone_number_for_password")
         user = get_object_or_404(User, phone=phone_number)
         uid = urlsafe_base64_encode(force_bytes(user))
         token = default_token_generator.make_token(user)
-        del self.request.session['phone_number_for_password']
-        return redirect(reverse('phone_login:change_password', kwargs={'uidb64': uid, 'token': token}))
+        del self.request.session["phone_number_for_password"]
+        return redirect(
+            reverse(
+                "phone_login:change_password", kwargs={"uidb64": uid, "token": token}
+            )
+        )
 
-    @method_decorator(phone_number_require('forget_password', 'phone_number_for_password'))
+    @method_decorator(
+        phone_number_require("forget_password", "phone_number_for_password")
+    )
     def dispatch(self, *args, **kwargs):
         return super().dispatch(*args, **kwargs)
 
 
 class ChangePassword(FormView):
     form_class = SetPasswordForm
-    template_name = 'registration/change_password.html'
-    success_url = '/'
+    template_name = "registration/change_password.html"
+    success_url = "/"
 
     def get_user(self, uidb64):
         try:
             # urlsafe_base64_decode() decodes to bytestring
             uid = urlsafe_base64_decode(uidb64).decode()
             user = User._default_manager.get(phone=uid)
-        except (TypeError, ValueError, OverflowError, User.DoesNotExist, ValidationError):
+        except (
+            TypeError,
+            ValueError,
+            OverflowError,
+            User.DoesNotExist,
+            ValidationError,
+        ):
             user = None
         return user
 
     def get(self, request, *args, **kwargs):
-        user = self.get_user(kwargs['uidb64'])
-        if user is not None and default_token_generator.check_token(user, self.kwargs.get('token')):
+        user = self.get_user(kwargs["uidb64"])
+        if user is not None and default_token_generator.check_token(
+            user, self.kwargs.get("token")
+        ):
             return super().get(request, *args, **kwargs)
-        return HttpResponse('Invalid Link')
+        return HttpResponse("Invalid Link")
 
     def post(self, request, *args, **kwargs):
-        user = self.get_user(kwargs['uidb64'])
+        user = self.get_user(kwargs["uidb64"])
         form = self.form_class(user=user, data=request.POST)
         if form.is_valid():
             return self.form_valid(form)
@@ -193,7 +216,11 @@ class ChangePassword(FormView):
 
     def form_valid(self, form):
         user = form.save()
-        auth = authenticate(username=user.phone, password=form.cleaned_data['new_password2'])
+        auth = authenticate(
+            username=user.phone, password=form.cleaned_data["new_password2"]
+        )
         if auth:
-            login(self.request, user, backend='django.contrib.auth.backends.ModelBackend')
+            login(
+                self.request, user, backend="django.contrib.auth.backends.ModelBackend"
+            )
         return super().form_valid(form)
